f5225d02c812352f0b88739bc46657a7d2c408f2
Trisha Gee <trisha.gee@gmail.com>
2019-12-14 17:47:05 +0100

    Using FXWeaver significantly simplifies the StageInitializer

--- stock-ui/src/main/java/com/mechanitis/demo/stockui/StageInitializer.java
+++ stock-ui/src/main/java/com/mechanitis/demo/stockui/StageInitializer.java
@@ -1,44 +1,29 @@
 package com.mechanitis.demo.stockui;
 
 import com.mechanitis.demo.stockui.ChartApplication.StageReadyEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.rgielen.fxweaver.core.FxWeaver;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 public class StageInitializer implements ApplicationListener<StageReadyEvent> {
-    @Value("classpath:/chart.fxml")
-    private Resource chartResource;
-    private String applicationTitle;
-    private ApplicationContext applicationContext;
+    private final String applicationTitle;
+    private final FxWeaver fxWeaver;
 
     public StageInitializer(@Value("${spring.application.ui.title}") String applicationTitle,
-                            ApplicationContext applicationContext) {
+                            FxWeaver fxWeaver) {
         this.applicationTitle = applicationTitle;
-        this.applicationContext = applicationContext;
+        this.fxWeaver = fxWeaver;
     }
 
     @Override
     public void onApplicationEvent(StageReadyEvent event) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(chartResource.getURL());
-            fxmlLoader.setControllerFactory(aClass -> applicationContext.getBean(aClass));
-            Parent parent = fxmlLoader.load();
-
-            Stage stage = event.getStage();
-            stage.setScene(new Scene(parent, 800, 600));
-            stage.setTitle(applicationTitle);
-            stage.show();
-        } catch (IOException e) {
-            throw new RuntimeException();
-        }
+        Stage stage = event.getStage();
+        stage.setScene(new Scene(fxWeaver.loadView(ChartController.class), 800, 600));
+        stage.setTitle(applicationTitle);
+        stage.show();
     }
 }
